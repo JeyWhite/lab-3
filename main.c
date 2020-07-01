d47c940d64b692716b5d706380c851cf9a80ab92
Jay White <evg.b42@ya.ru>
2020-07-02 01:42:15 +0700

    every beginnig is beutiful

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hi!\n");
+for (size_t i = 1; i < 6; i++)
+    {
+
+        for (size_t j = 0; j < 6; j++)
+        {
+                    if(i<3)
+                    {
+                        printf(" ");
+                    }
+                    else
+                    {
+                        printf("*");
+                    }
+                    
+        }
+        printf("\n");
+    }
+    
+}
